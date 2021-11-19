12eb620daf194938e099503c812dd77bdc583bf0
Adrien Fabre <adrien.fabre@dashlane.com>
2021-11-19 23:43:19 +0100

    Add colors

--- src/Ant.ts
+++ src/Ant.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import {map} from './utils';
 
 type Coordinates = {
     x: number;
@@ -35,33 +36,39 @@ const getNewDirection = (currentDirection: Direction, rotation: 'clockwise' | 'c
 };
 
 export class Grid {
-    cells: boolean[][];
+    cells: number[][];
     app: PIXI.Application;
     w: number;
     h: number;
     antPos: Coordinates;
     antDirection: Direction;
-    maxAge: number;
+    iteration: number;
+    scaleX: number;
+    scaleY: number;
 
     constructor(w: number, h: number, app: PIXI.Application) {
         this.app = app;
         this.w = w;
         this.h = h;
+        this.scaleX = this.app.view.width / this.w;
+        this.scaleY = this.app.view.height / this.h;
+
         this.antPos = {x: Math.floor(w / 2), y: Math.floor(h / 2)};
         this.antDirection = 'down';
-        this.maxAge = 0;
+        this.iteration = 0;
         this.cells = [];
         for (let y = 0; y < h; y++) {
             const line = [];
             for (let x = 0; x < w; x++) {
-                line.push(false);
+                line.push(0);
             }
             this.cells.push(line);
         }
     }
 
     update() {
-        const rotation = this.cells[this.antPos.y][this.antPos.x] ? 'clockwise' : 'counter';
+        this.iteration++;
+        const rotation = this.cells[this.antPos.y][this.antPos.x] !== 0 ? 'clockwise' : 'counter';
         this.antDirection = getNewDirection(this.antDirection, rotation);
         switch (this.antDirection) {
             case 'up':
@@ -88,13 +95,14 @@ export class Grid {
             this.antPos.y = 0;
         }
 
-        this.cells[this.antPos.y][this.antPos.x] = !this.cells[this.antPos.y][this.antPos.x];
+        if (this.cells[this.antPos.y][this.antPos.x]) {
+            this.cells[this.antPos.y][this.antPos.x] = 0;
+        } else {
+            this.cells[this.antPos.y][this.antPos.x] = this.iteration;
+        }
     }
 
     draw() {
-        const scaleX = this.app.view.width / this.w;
-        const scaleY = this.app.view.height / this.h;
-
         let backgroundGraphics = new PIXI.Graphics();
         const emptyColor = 0xffffff;
         backgroundGraphics.beginFill(emptyColor);
@@ -104,23 +112,28 @@ export class Grid {
         const fullColor = 0x010101;
         for (let x = 0; x < this.w; x++) {
             for (let y = 0; y < this.h; y++) {
-                if (!this.cells[y][x]) {
+                if (this.cells[y][x] === 0) {
                     continue;
                 }
 
+                const level = map(this.cells[y][x], 0, this.iteration, 250, 0);
+                const levelHex = level.toString(16);
+                const colorHex = `#${levelHex}${levelHex}${levelHex}`;
+                const color = parseInt(colorHex.substring(1), 16);
+
                 const graphics = new PIXI.Graphics();
-                graphics.beginFill(fullColor);
-                graphics.drawRect(x * scaleX, y * scaleY, scaleX, scaleY);
+                graphics.beginFill(color);
+                graphics.drawRect(x * this.scaleX, y * this.scaleY, this.scaleX, this.scaleY);
                 this.app.stage.addChild(graphics);
             }
         }
 
         const ant = new PIXI.Sprite(texture);
-        ant.x = this.antPos.x * scaleX + scaleX / 2;
-        ant.y = this.antPos.y * scaleY + scaleY / 2;
+        ant.x = this.antPos.x * this.scaleX + this.scaleX / 2;
+        ant.y = this.antPos.y * this.scaleY + this.scaleY / 2;
         ant.anchor.set(0.5);
-        const antScaleX = scaleX / ant.width;
-        const antScaleY = scaleY / ant.height;
+        const antScaleX = this.scaleX / ant.width;
+        const antScaleY = this.scaleY / ant.height;
         ant.scale.set(antScaleX, antScaleY);
 
         if (this.antDirection === 'right') {
